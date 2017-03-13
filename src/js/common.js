d42127bf39c078c7955ff3c90ca234c11d62fec9
Re-z <re-z@ukr.net>
2017-03-13 17:34:52 +0200

    testing

--- src/js/common.js
+++ src/js/common.js
@@ -119,4 +119,24 @@ function gallerySlider() {
 gallerySlider();
 
 
+//scrollTop
+function runScroll() {
+    scrollTo(document.body, 0, 400);
+}
+var scrollme;
+scrollme = document.querySelector(".footer__to-top");
+scrollme.addEventListener("click",runScroll,false);
+
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop == to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+
 
